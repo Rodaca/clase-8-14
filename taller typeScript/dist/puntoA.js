adf544387d81aa63fc1069b7a80ff6dc7ade70ff
rodacay <leapardo4@gmail.com>
2023-08-16 13:53:41 -0500

    feat: :sparkles: se desarollan punto 3 y 5

--- taller typeScript/dist/puntoA.js
+++ taller typeScript/dist/puntoA.js
@@ -45,7 +45,7 @@ const nivel5 = new Nivel(5, "Frameworks de Desarrollo", 5, 9);
 const nivel6 = new Nivel(6, "Inteligencia Artificial Aplicada", 6, 12);
 const nivel7 = new Nivel(7, "Proyecto Final: Aplicación Web Avanzada", 7, 15);
 class Camper {
-    constructor(objectId, nombre, típoIdentificacion, NroIdentificacion, email, password, level, levelState, estado, imagen, promedio) {
+    constructor(objectId, nombre, típoIdentificacion, NroIdentificacion, email, password, level, levelState, estado, imagen, promedio, centro) {
         this.objectId = objectId;
         this.nombre = nombre;
         this.típoIdentificacion = típoIdentificacion;
@@ -57,15 +57,81 @@ class Camper {
         this.estado = estado;
         this.imagen = imagen;
         this.promedio = promedio;
+        this.centro = centro;
+    }
+    gestionarContrato() {
+        if (this.level == 7 && this.levelState == "completado") {
+            console.log(`El camper ${this.nombre} es contratado`);
+        }
+        else {
+            console.log(`El camper ${this.nombre}  un le falta terminar el proceso`);
+        }
+    }
+    contarCentros() {
+        let var1 = 0, var2 = 0, var3 = 0, var4 = 0, var5 = 0, var6 = 0, var7 = 0;
+        let nom1 = "Centro IT Innovate", nom2 = "Academia CodeTech", nom3 = "Instituto DigitalTech", nom4 = "Centro Informático ByteMaster", nom5 = "Escuela TechCode", nom6 = "Academia CodeWave", nom7 = "Instituto IT Future";
+        let nombreM = "";
+        let nombrem = "";
+        let valorM = 0;
+        let valorm = 10000000;
+        camperes.forEach(element => {
+            switch (element.centro) {
+                case "Centro IT Innovate":
+                    var1 += 1;
+                    break;
+                case "Academia CodeTech":
+                    var2 += 1;
+                    break;
+                case "Instituto DigitalTech":
+                    var3 += 1;
+                    break;
+                case "Centro Informático ByteMaster":
+                    var4 += 1;
+                    break;
+                case "Escuela TechCode":
+                    var5 += 1;
+                    break;
+                case "Academia CodeWave":
+                    var6 += 1;
+                    break;
+                case "Instituto IT Future":
+                    var7 += 1;
+                    break;
+                default:
+                    break;
+            }
+            let variables = [var1, var2, var3, var4, var5, var6, var7];
+            let nombres = [nom1, nom2, nom3, nom4, nom5, nom6, nom7];
+            variables.forEach((element, idx) => {
+                if (element > valorM) {
+                    valorM = element;
+                    nombreM = nombres[idx];
+                }
+                if (element < valorm) {
+                    valorm = element;
+                    nombrem = nombres[idx];
+                }
+            });
+        });
+        console.log(`El centro ${nombreM} con ${valorM} y el que menos ${nombrem} con ${valorm}`);
     }
 }
-const camper1 = new Camper(1, "Juan Pérez", "Cédula", "123456789", "juan@example.com", "clave123", 4, "en proceso", true, "imagen1.jpg", 85.5);
-const camper2 = new Camper(2, "Ana García", "Cédula", "987654321", "ana@example.com", "password456", 2, "completado", true, "imagen2.jpg", 70.0);
-const camper3 = new Camper(3, "Luisa Rodríguez", "Pasaporte", "ABCD1234", "luisa@example.com", "secret123", 7, "en proceso", true, "imagen3.jpg", 92.3);
-const camper4 = new Camper(4, "Carlos Martínez", "Cédula", "456789123", "carlos@example.com", "contraseña", 5, "completado", true, "imagen4.jpg", 78.9);
-const camper5 = new Camper(5, "María López", "Cédula", "567891234", "maria@example.com", "pass123", 3, "en proceso", false, "imagen5.jpg", 60.7);
-const camper6 = new Camper(6, "Andrés Ramírez", "Cédula", "345678912", "andres@example.com", "securepwd", 1, "en proceso", true, "imagen6.jpg", 42.0);
-const camper7 = new Camper(7, "Laura González", "Cédula", "678912345", "laura@example.com", "clave321", 6, "completado", true, "imagen7.jpg", 88.2);
+const camper1 = new Camper(1, "Juan Pérez", "Cédula", "123456789", "juan@example.com", "clave123", 4, "en proceso", true, "imagen1.jpg", 85.5, "Academia CodeTech");
+const camper2 = new Camper(2, "Ana García", "Cédula", "987654321", "ana@example.com", "password456", 2, "completado", true, "imagen2.jpg", 70.0, "Escuela TechCode");
+const camper3 = new Camper(3, "Luisa Rodríguez", "Pasaporte", "ABCD1234", "luisa@example.com", "secret123", 7, "completado", true, "imagen3.jpg", 92.3, "Centro IT Innovate");
+const camper4 = new Camper(4, "Carlos Martínez", "Cédula", "456789123", "carlos@example.com", "contraseña", 5, "completado", true, "imagen4.jpg", 78.9, "Academia CodeTech");
+const camper5 = new Camper(5, "María López", "Cédula", "567891234", "maria@example.com", "pass123", 3, "en proceso", false, "imagen5.jpg", 60.7, "Academia CodeTech");
+const camper6 = new Camper(6, "Andrés Ramírez", "Cédula", "345678912", "andres@example.com", "securepwd", 1, "en proceso", true, "imagen6.jpg", 42.0, "Centro IT Innovate");
+const camper7 = new Camper(7, "Laura González", "Cédula", "678912345", "laura@example.com", "clave321", 6, "completado", true, "imagen7.jpg", 88.2, "Escuela TechCode");
+const camperes = [
+    camper1,
+    camper2,
+    camper3,
+    camper4,
+    camper5,
+    camper6,
+    camper7,
+];
 class Contrato {
     constructor(object_id, nombre, descripción, estado, tipoContrato, camper //en vez de usar  Camper.object_id usaria Camper.NroIdentificacion
     ) {
@@ -75,11 +141,6 @@ class Contrato {
         this.estado = estado;
         this.tipoContrato = tipoContrato;
         this.camper = camper;
-    }
-    gestionarContrato(camper) {
-        if (true) {
-            console.log("esta contratado");
-        }
     }
     tipoDeContrato() {
         let cantRemoto = 0;
@@ -150,5 +211,9 @@ const contrato4 = new Contrato(4, "Contrato de Capacitación en Programación", 
 const contrato5 = new Contrato(5, "Contrato de Desarrollo de App Móvil", "Creación de una aplicación móvil.", "No contratado", "remoto", "567891234");
 const contrato6 = new Contrato(6, "Contrato de Implementación ERP", "Implementación de sistema de planificación empresarial.", "No contratado", "presencial", "345678912");
 const contrato7 = new Contrato(7, "Contrato de Desarrollo de Juego", "Creación de un videojuego educativo.", "No contratado", "remoto", "678912345");
-console.log(contrato2.gestionarContrato(camper2));
-console.log(contrato1.tipoDeContrato());
+//3. Gestionar la contratación del camper que solamente a terminado cada niveldelaruta.
+camper3.gestionarContrato();
+//4. Reportar campers que obtuvieron contrato remoto y campers que obtuvieroncontratopresencial,
+contrato1.tipoDeContrato();
+//5. Centro de entrenamiento con mayor cantidad de campers y con menor cantidaddecampers
+camper5.contarCentros();
--- taller typeScript/src/puntoA.ts
+++ taller typeScript/src/puntoA.ts
@@ -60,17 +60,93 @@ class Camper{
         public estado:boolean,
         public imagen:string,
         public promedio:number,
+        public centro:string,
         ){}
+    public gestionarContrato() {
+        if (this.level==7 && this.levelState=="completado"){ 
+            console.log(`El camper ${this.nombre} es contratado`);
+        }else{
+            console.log(`El camper ${this.nombre}  un le falta terminar el proceso`);
+        }
+    }
+    public contarCentros(){
+        let var1:number=0,var2:number=0,var3:number=0,var4:number=0,var5:number=0,var6:number=0,var7:number=0;
+        let nom1:string="Centro IT Innovate",
+            nom2:string="Academia CodeTech",
+            nom3:string="Instituto DigitalTech",
+            nom4:string="Centro Informático ByteMaster",
+            nom5:string="Escuela TechCode",
+            nom6:string="Academia CodeWave",
+            nom7:string="Instituto IT Future"
+        let nombreM:string =""
+        let nombrem:string = ""
+        let valorM:number=0
+        let valorm:number=10000000
+        camperes.forEach(element => {
+           switch (element.centro) {
+            case "Centro IT Innovate":
+                var1+=1
+                
+                break;
+            case "Academia CodeTech":
+                var2+=1
+                break;
+            case "Instituto DigitalTech":
+                var3+=1
+                break;
+            case "Centro Informático ByteMaster":
+                var4+=1
+                break;
+            case "Escuela TechCode":
+                var5+=1
+                break;
+            case "Academia CodeWave":
+                var6+=1
+                break;
+            case "Instituto IT Future":
+                var7+=1
+                break;
+            default:
+                break;
+           }
+        let variables:number[]=[var1,var2,var3,var4,var5,var6,var7]
+        let nombres:string[]=[nom1,nom2,nom3,nom4,nom5,nom6,nom7]
+        
+        variables.forEach((element,idx) => {
+            if (element>valorM) {
+                valorM=element
+                nombreM=nombres[idx]
+            }
+            if (element<valorm) {
+                valorm=element
+                nombrem=nombres[idx]
+            }
+
+        });  
+        });
+        
+        console.log(`El centro ${nombreM} con ${valorM} y el que menos ${nombrem} con ${valorm}`);
+        
+    }
+    
+        
 }
 
-const camper1 = new Camper(1, "Juan Pérez", "Cédula", "123456789", "juan@example.com", "clave123", 4, "en proceso", true, "imagen1.jpg", 85.5);
-const camper2 = new Camper(2, "Ana García", "Cédula", "987654321", "ana@example.com", "password456", 2, "completado", true, "imagen2.jpg", 70.0);
-const camper3 = new Camper(3, "Luisa Rodríguez", "Pasaporte", "ABCD1234", "luisa@example.com", "secret123", 7, "en proceso", true, "imagen3.jpg", 92.3);
-const camper4 = new Camper(4, "Carlos Martínez", "Cédula", "456789123", "carlos@example.com", "contraseña", 5, "completado", true, "imagen4.jpg", 78.9);
-const camper5 = new Camper(5, "María López", "Cédula", "567891234", "maria@example.com", "pass123", 3, "en proceso", false, "imagen5.jpg", 60.7);
-const camper6 = new Camper(6, "Andrés Ramírez", "Cédula", "345678912", "andres@example.com", "securepwd", 1, "en proceso", true, "imagen6.jpg", 42.0);
-const camper7 = new Camper(7, "Laura González", "Cédula", "678912345", "laura@example.com", "clave321", 6, "completado", true, "imagen7.jpg", 88.2);
-
+const camper1 = new Camper(1, "Juan Pérez", "Cédula", "123456789", "juan@example.com", "clave123", 4, "en proceso", true, "imagen1.jpg", 85.5,"Academia CodeTech");
+const camper2 = new Camper(2, "Ana García", "Cédula", "987654321", "ana@example.com", "password456", 2, "completado", true, "imagen2.jpg", 70.0,"Escuela TechCode");
+const camper3 = new Camper(3, "Luisa Rodríguez", "Pasaporte", "ABCD1234", "luisa@example.com", "secret123", 7, "completado", true, "imagen3.jpg", 92.3,"Centro IT Innovate");
+const camper4 = new Camper(4, "Carlos Martínez", "Cédula", "456789123", "carlos@example.com", "contraseña", 5, "completado", true, "imagen4.jpg", 78.9,"Academia CodeTech");
+const camper5 = new Camper(5, "María López", "Cédula", "567891234", "maria@example.com", "pass123", 3, "en proceso", false, "imagen5.jpg", 60.7,"Academia CodeTech");
+const camper6 = new Camper(6, "Andrés Ramírez", "Cédula", "345678912", "andres@example.com", "securepwd", 1, "en proceso", true, "imagen6.jpg", 42.0,"Centro IT Innovate");
+const camper7 = new Camper(7, "Laura González", "Cédula", "678912345", "laura@example.com", "clave321", 6, "completado", true, "imagen7.jpg", 88.2,"Escuela TechCode");
+const camperes =[
+    camper1,
+    camper2,
+    camper3, 
+    camper4, 
+    camper5, 
+    camper6, 
+    camper7, ]
 class Contrato {
     constructor(
         public object_id:number,
@@ -81,15 +157,6 @@ class Contrato {
         public camper: string //en vez de usar  Camper.object_id usaria Camper.NroIdentificacion
     ) 
     {}
-
-    public gestionarContrato(camper:Camper) {
-        if (camper.level==7 && camper.levelState=="completado") {
-            console.log(`El camper ${camper.nombre} es contratado en ${this.nombre}`);
-            this.estado="Contratado"
-        }
-    }
-    
-    
     public tipoDeContrato(){
         let cantRemoto:number=0;
         let cantPresencial:number=0;
@@ -164,13 +231,10 @@ const contrato6 = new Contrato(6, "Contrato de Implementación ERP", "Implementa
 const contrato7 = new Contrato(7, "Contrato de Desarrollo de Juego", "Creación de un videojuego educativo.", "No contratado","remoto", "678912345");
 
 
-
-
-
-
-
-
-
-console.log(contrato2.gestionarContrato(camper2));
-console.log(contrato1.tipoDeContrato());
+//3. Gestionar la contratación del camper que solamente a terminado cada niveldelaruta.
+camper3.gestionarContrato();
+//4. Reportar campers que obtuvieron contrato remoto y campers que obtuvieroncontratopresencial,
+contrato1.tipoDeContrato();
+//5. Centro de entrenamiento con mayor cantidad de campers y con menor cantidaddecampers
+camper5.contarCentros()
 
